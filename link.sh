a36e69f9dc9cc7d101493c58c095109c2cf71f9d
MrLoh <flojoto@hotmail.com>
2017-03-12 16:01:34 +0100

    change location of configs folder

--- link.sh
+++ link.sh
@@ -2,19 +2,19 @@
 
 # Git
 rm ~/.gitconfig
-ln -s ~/configs/gitconfig ~/.gitconfig
+ln -s ~/Repos/_configs/gitconfig ~/.gitconfig
 
 rm ~/.gitignore_global
-ln -s ~/configs/gitignore ~/.gitignore_global
+ln -s ~/Repos/_configs/gitignore ~/.gitignore_global
 
 # fish
 rm ~/.config/fish/config.fish
-ln -s ~/configs/fishconfig ~/.config/fish/config.fish
+ln -s ~/Repos/_configs/fishconfig ~/.config/fish/config.fish
 
 rm -rf ~/.config/fish/functions
-ln -s ~/configs/fishfunctions ~/.config/fish/functions
+ln -s ~/Repos/_configs/fishfunctions ~/.config/fish/functions
 
 # bash
 rm ~/.bashrc
-ln -s ~/configs/bashrc ~/.bashrc
+ln -s ~/Repos/_configs/bashrc ~/.bashrc
 
