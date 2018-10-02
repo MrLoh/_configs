212d46c29883503ad9f18d2a2b2d86fd4f8d1bda
MrLoh <flojoto@hotmail.com>
2018-10-02 15:48:04 -0500

    fix fish configs

--- link.sh
+++ link.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
 # Git
-rm ~/.gitconfig
-ln -s ./gitconfig ~/.gitconfig
+rm -f ~/.gitconfig
+ln -s $PWD/gitconfig ~/.gitconfig
 
-rm ~/.gitignore_global
-ln -s ./gitignore ~/.gitignore_global
+rm -f ~/.gitignore_global
+ln -s $PWD/gitignore ~/.gitignore_global
 
 # fish
-rm ~/.config/fish/config.fish
-ln -s ./fishconfig ~/.config/fish/config.fish
+rm -f ~/.config/fish/config.fish
+ln -s $PWD/fishconfig ~/.config/fish/config.fish
 
 rm -rf ~/.config/fish/functions
-ln -s ./fishfunctions ~/.config/fish/functions
+ln -s $PWD/fishfunctions ~/.config/fish/functions
 
 # bash
-rm ~/.bashrc
-ln -s ./bash_profile ~/.bash_profile
+rm -f ~/.bashrc
+rm -f ~/.bash_profile
+ln -s $PWD/bash_profile ~/.bash_profile
 
