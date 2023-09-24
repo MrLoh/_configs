f10a398b9f5cd04e3e4b6862aea272847a7dedc3
Tobias Lohse <MrLoh@users.noreply.github.com>
2023-09-24 14:24:16 -0500

    update asdf setup

--- setup.asdf.sh
+++ setup.asdf.sh
@@ -1,6 +1,6 @@
 asdf plugin add nodejs
-asdf install nodejs latest:16
-asdf global nodejs latest:16
+asdf install nodejs latest:18
+asdf global nodejs latest:18
 
 asdf plugin add ruby
 asdf install ruby latest
@@ -9,3 +9,7 @@ asdf global ruby latest
 asdf plugin add python
 asdf install python latest
 asdf global python latest
+
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
+asdg install terraform latest
+asdf global terraform latest
