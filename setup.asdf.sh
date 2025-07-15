a989af2bee5badd82132e4818674e2cee817d334
Tobias Lohse <MrLoh@users.noreply.github.com>
2025-07-15 09:24:58 -0500

    manage poetry with asdf

--- setup.asdf.sh
+++ setup.asdf.sh
@@ -1,3 +1,5 @@
+asdf completion fish > ~/.config/fish/completions/asdf.fish
+
 asdf plugin add nodejs
 asdf install nodejs latest:18
 asdf global nodejs latest:18
@@ -9,6 +11,10 @@ asdf global ruby latest
 asdf plugin add python
 asdf install python latest:12
 asdf global python latest:12
+
+asdf plugin add poetry https://github.com/asdf-community/asdf-poetry.git
+asdf install poetry latest
+asdf global poetry latest
 
 asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 asdg install terraform latest
